380558da0f9e3f360c6ee80a835c86d6ba71d19d
X2isme <98345602+X2isme@users.noreply.github.com>
2022-06-25 16:27:22 -0700

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -53,7 +53,7 @@ end
         game.Players.LocalPlayer.RemoteFunctions.UR_ANTIHACK:FireServer("Strength", "one", 9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999, 2)
     end)
 
-    MiscSection:NewButton("Reset Gold", "HIT A NPC TO RESET GOLD", function()
+    MiscSection:NewButton("Reset Gold", "KILL A NPC TO RESET GOLD", function()
         game.Players.LocalPlayer.RemoteFunctions.UR_ANTIHACK:FireServer("Loot", "one", 9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999, 2)
     end)
 end
@@ -241,5 +241,64 @@ if game.PlaceId == 6668183009 then
 
     MainSection:NewTextBox("Money", "Press Enter After Value, Money Put In Money Collector", function(Monera)
         game.ReplicatedStorage.Shop:FireServer("Buy", "J", -Monera)
+    end)
+end
+
+if game.PlaceId == 6668183009 then
+        -- MAIN
+    local Main = Window:NewTab("Main")
+    local MainSection = Main:NewSection("Main")
+
+    MiscSection:NewButton("SEE PAINTINGS WORTH", "Press F9 and look at warnings", function()
+       local DevConsole = game:GetService("CoreGui"):WaitForChild("DevConsoleMaster")
+local Window = DevConsole.DevConsoleWindow
+local UI = Window.DevConsoleUI
+local MainView = UI:WaitForChild("MainView")
+local ClientLog = MainView:WaitForChild("ClientLog")
+
+local GetHighest = function(...)
+local HighestValue
+for _, Value in next, ... do
+if not HighestValue or Value > HighestValue then
+HighestValue = Value
+end
+end
+return HighestValue
+end
+
+local CPrint = function(...)
+local Args = {...}
+local TempColour = table.remove(Args)
+local Colour
+if typeof(TempColour) == "Color3" then
+Colour = TempColour
+else
+Colour = Color3.fromRGB(255,255,255)
+table.insert(Args, TempColour)
+end
+local CandidParts = {}
+for _, Element in next, ClientLog:GetChildren() do
+local Name = Element.Name
+if Name:match("%d+") then
+table.insert(CandidParts, tonumber(Element.Name))
+end
+end
+   local Expecting = GetHighest(CandidParts)
+   Expecting = Expecting and (Expecting + 1) or (2)
+   print(unpack(Args))
+local ExpectedPart = ClientLog:WaitForChild(Expecting)
+   local MessagePart = ExpectedPart:FindFirstChild("msg")
+   if MessagePart then
+       MessagePart.TextColor3 = Colour
+   end
+end
+
+-- Script Made by Unordinary
+for i,v in pairs(game:GetService("ReplicatedStorage").PlayerInfo:GetDescendants()) do
+    if v.Name == "1" or v.Name == "2" or v.Name == "3" then
+        CPrint(v.Value, Color3.fromRGB(0,255,109))
+        wait()
+    end
+end 
     end)
 end
