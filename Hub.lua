9fdc165ef5f2873ea6966e6122be81bb2d75dece
X2isme <98345602+X2isme@users.noreply.github.com>
2022-07-03 18:19:15 -0700

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -646,12 +646,9 @@ local MainSection = Main:NewSection("Main")
     end)
 end
 
-if game.PlaceId => 1 then
-    -- MAIN
+
 local All = Window:NewTab("All")
 local AllSection = All:NewSection("All Games")
-
     MainSection:NewButton("Copy Console", "Console has to be open", function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/X2isme/Roblox-Hub/main/console.lua",true))() 
     end)
-end
