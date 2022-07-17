e825555088080c6e2a3307f61614c7896cf07466
X2isme <98345602+X2isme@users.noreply.github.com>
2022-07-16 18:16:56 -0700

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -847,6 +847,35 @@ local AllSection = All:NewSection("All Games")
         loadstring(game:HttpGet("https://raw.githubusercontent.com/X2isme/Roblox-Hub/main/AnimationEditGUI.lua",true))()
     end)
 
+    AllSection:NewButton("GodMode", "Works in only some games", function()
+        game.Players.LocalPlayer.Character.Humanoid.Name = 1
+        local l = game.Players.LocalPlayer.Character["1"]:Clone()
+        l.Parent = game.Players.LocalPlayer.Character
+        l.Name = "Humanoid"
+        wait(0.1)
+        game.Players.LocalPlayer.Character["1"]:Destroy()
+        game.Workspace.CurrentCamera.CameraSubject = game.Players.LocalPlayer.Character
+        game.Players.LocalPlayer.Character.Animate.Disabled = true
+        wait(0.1)
+        game.Players.LocalPlayer.Character.Animate.Disabled = false
+        game.Players.LocalPlayer.Character.Humanoid.DisplayDistanceType = "None"
+    end)
+
+    AllSection:NewDropdown("Sploof Device", "Works in only some games", {"Mobile", "Gamepad", "Keyboard"}, function(Device)
+        local old
+        local DeviceToSpoof = "Gamepad"
+
+        old = hookmetamethod(game,"__namecall",function(self,...)
+        local args = {...}
+        local method = getnamecallmethod()
+        if method == "FireServer" then
+            if tostring(self) == "deviceEvent" then
+                args[1] = DeviceToSpoof
+                return self.FireServer(self,unpack(args))
+            end) 
+    end)
+
+    
 
 
     -- Hubs
