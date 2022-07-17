d26ce64bc7218e71b168e97675b0ebd25aded6a3
X2isme <98345602+X2isme@users.noreply.github.com>
2022-07-16 18:31:13 -0700

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -863,16 +863,19 @@ local AllSection = All:NewSection("All Games")
 
     AllSection:NewDropdown("Sploof Device", "Works in only some games", {"Mobile", "Gamepad", "Keyboard"}, function(Device)
         local old
-        local DeviceToSpoof = "Gamepad"
+local DeviceToSpoof = Device
 
-        old = hookmetamethod(game,"__namecall",function(self,...)
-        local args = {...}
-        local method = getnamecallmethod()
-        if method == "FireServer" then
-            if tostring(self) == "deviceEvent" then
-                args[1] = DeviceToSpoof
-                return self.FireServer(self,unpack(args))
-            end) 
+old = hookmetamethod(game,"__namecall",function(self,...)
+   local args = {...}
+   local method = getnamecallmethod()
+   if method == "FireServer" then
+       if tostring(self) == "deviceEvent" then
+           args[1] = DeviceToSpoof
+           return self.FireServer(self,unpack(args))
+       end
+   end
+   return old(self,...)
+end)
     end)
 
     
