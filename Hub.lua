e4cdd08628f98f18e129324cbbe9ebbc69092ca2
X2isme <98345602+X2isme@users.noreply.github.com>
2022-08-16 12:39:43 -0700

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -424,135 +424,7 @@ end)
     Players = game:GetService("Players")
     for i, player in pairs(Players:GetPlayers()) do
         MainSection:NewDropdown("Max Base", "Give a MaxedOut base to anyone!", {Players}, function(Victim)
-            local chosenPlayer = Victim
-
-for i,v in pairs(game.Workspace.Game.Buttons:GetChildren()) do
-	if v.Name == chosenPlayer then
-		for i,v2 in pairs(v:GetChildren()) do
-			for i,v3 in pairs(v2:GetChildren()) do
-				if v3:IsA("ObjectValue") then
-					v3.Name = v3.Value.Name
-				end
-			end
-		end
-	end
-end
-
-
-for i,v in pairs(game.Workspace.Game.Buttons:GetChildren()) do
-	if v.Name == chosenPlayer then
-		for i,v2 in pairs(v:GetChildren()) do
-			for i,v3 in pairs(v2:GetChildren()) do
-				if v3:IsA("ObjectValue") then
-					if v3.Name == "Barracks" then
-						local Class = game.ReplicatedStorage.Game.Buildings["Barracks"]["2"];
-						local Button = v2;
-						local Target = game:GetService("ReplicatedStorage").RE.insertBuilding;
-						Target:FireServer(Class, Button);
-					else
-						if v3.Name == "Greenhouse" then
-							local Class = game.ReplicatedStorage.Game.Buildings["Greenhouse"]["2"];
-							local Button = v2;
-							local Target = game:GetService("ReplicatedStorage").RE.insertBuilding;
-							Target:FireServer(Class, Button);
-						else
-							if v3.Name == "Factory" then
-									local Class = game.ReplicatedStorage.Game.Buildings["Factory"]["3"];
-									local Button = v2;
-									local Target = game:GetService("ReplicatedStorage").RE.insertBuilding;
-									Target:FireServer(Class, Button);
-							else
-								if v3.Name == "Oil Field" then
-										local Class = game.ReplicatedStorage.Game.Buildings["Oil Field"]["2"];
-										local Button = v2;
-										local Target = game:GetService("ReplicatedStorage").RE.insertBuilding;
-										Target:FireServer(Class, Button);
-								else
-									if v3.Name == "Guard Tower" then
-										local Class = game.ReplicatedStorage.Game.Buildings["Guard Tower"]["1"];
-										local Button = v2;
-										local Target = game:GetService("ReplicatedStorage").RE.insertBuilding;
-										Target:FireServer(Class, Button);
-									else
-										if v3.Name == "Wall" then
-											local Class = game.ReplicatedStorage.Game.Buildings["Wall"]["2"];
-											local Button = v2;
-											local Target = game:GetService("ReplicatedStorage").RE.insertBuilding;
-											Target:FireServer(Class, Button);
-										else
-											if v3.Name == "Generator Powerplant" then
-												local Class = game.ReplicatedStorage.Game.Buildings["Generator Powerplant"]["1"];
-												local Button = v2;
-												local Target = game:GetService("ReplicatedStorage").RE.insertBuilding;
-												Target:FireServer(Class, Button);
-											else
-												if v3.Name == "Missile Factory" then
-													local Class = game.ReplicatedStorage.Game.Buildings["Missile Factory"]["1"];
-													local Button = v2;
-													local Target = game:GetService("ReplicatedStorage").RE.insertBuilding;
-													Target:FireServer(Class, Button);
-												else
-													if v3.Name == "Command Center" then
-														local Class = game.ReplicatedStorage.Game.Buildings["Command Center"]["2"];
-														local Button = v2;
-														local Target = game:GetService("ReplicatedStorage").RE.insertBuilding;
-														Target:FireServer(Class, Button);
-													else
-														if v3.Name == "Drone Factory" then
-															local Class = game.ReplicatedStorage.Game.Buildings["Drone Factory"]["1"];
-															local Button = v2;
-															local Target = game:GetService("ReplicatedStorage").RE.insertBuilding;
-															Target:FireServer(Class, Button);
-														else
-															if v3.Name == "Military" then
-																local Class = game.ReplicatedStorage.Game.Buildings.Military["Tank Factory"]["2"];
-																local Button = v2;
-																local Target = game:GetService("ReplicatedStorage").RE.insertBuilding;
-																Target:FireServer(Class, Button);
-															else
-																if v3.Name == "Nuclear Powerplant" then
-																	local Class = game.ReplicatedStorage.Game.Buildings["Nuclear Powerplant"]["1"];
-																	local Button = v2;
-																	local Target = game:GetService("ReplicatedStorage").RE.insertBuilding;
-																	Target:FireServer(Class, Button);
-																else
-																	if v3.Name == "Airport" then
-																		local Class = game.ReplicatedStorage.Game.Buildings["Airport"]["1"];
-																		local Button = v2;
-																		local Target = game:GetService("ReplicatedStorage").RE.insertBuilding;
-																		Target:FireServer(Class, Button);
-																	else
-																		if v3.Name == "Helicopter Bay" then
-																			local Class = game.ReplicatedStorage.Game.Buildings["Helicopter Bay"]["2"];
-																			local Button = v2;
-																			local Target = game:GetService("ReplicatedStorage").RE.insertBuilding;
-																			Target:FireServer(Class, Button);
-																		else
-																			if v3.Name == "Main Base" then
-																				local Class = game.ReplicatedStorage.Game.Buildings["Main Base"]["2"];
-																				local Button = v2;
-																				local Target = game:GetService("ReplicatedStorage").RE.insertBuilding;
-																				Target:FireServer(Class, Button);
-																			end
-																		end
-																	end
-																end
-															end
-														end
-													end
-												end
-											end
-										end
-									end
-								end
-							end
-						end
-					end
-				end
-			end
-		end
-	end
-end
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/X2isme/Roblox-Hub/main/MaxBaseAnyoneArmyTycoon.lua",true))()
     end)
     end
 end
@@ -1378,6 +1250,16 @@ MainSection:NewButton("Unlimited Steps", "Dont Lose Steps", function()
 
 MainSection:NewButton("Sword Reach", "OP", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/X2isme/Roblox-Hub/main/Reech.lua",true))()
+    end)
+end
+
+if game.PlaceId == 13822889 then
+    -- MAIN
+local Main = Window:NewTab("Main")
+local MainSection = Main:NewSection("Main")
+
+MainSection:NewButton("AzureHub Read Desc", "Key: Azure_crackedlol", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/X2isme/Roblox-Hub/main/AzureHub.lua",true))()
     end)
 end
 
