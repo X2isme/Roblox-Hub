3c2243da7a83c6df22b55e7632e31001c2d12938
X2isme <98345602+X2isme@users.noreply.github.com>
2022-08-16 16:05:28 -0700

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -985,52 +985,51 @@ if game.PlaceId == 9846056789 then
 local Main = Window:NewTab("Main")
 local MainSection = Main:NewSection("Main")
 
-
-Section:NewLabel("Cups Minigame")
-
-MainSection:NewTextBox("Cup1 - Desc", "Highlight a Cup 1-5", function(DiamondCup1)
-    FirstDiamondCup = DiamondCup1
-    -- Code
-    local Descendants = workspace:GetDescendants()
-    for i, value in pairs(Descendants) do
-        if value.Name == "Important" then
-            local cups = value:GetDescendants()
-            for i, e in pairs (cups) do
-                if e.Name == "Detail" then
-                    local v = e.Parent.Parent
-                    local c = v:GetChildren()
-                    for i, va in pairs(c) do
-                        local s = va.Spot
-                        va.Name = tostring(s.Value)
-                    end
-                    v[FirstDiamondCup].Detail.Color = Color3.fromRGB(0,191,255)
-                    v[FirstDiamondCup].Primary.Color = Color3.fromRGB(0,191,248)
-                end
-            end
-         end  
+MainSection:NewButton("Diamond Cup ESP", "press when diamonds r shown", function(DiamondCup1)
+    for i,v in pairs(workspace:GetChildren()) do
+        if v.Name == "fakediamond1" then v:Destroy() end
     end
-    end)
-
-MainSection:NewTextBox("Cup2 - Desc", "Highlight a Cup 1-5", function(DiamondCup2)
-    SecondDiamondCup = DiamondCup2
-    -- Code
-    local Descendants = workspace:GetDescendants()
-    for i, value in pairs(Descendants) do
-        if value.Name == "Important" then
-            local cups = value:GetDescendants()
-            for i, e in pairs (cups) do
-                if e.Name == "Detail" then
-                    local v = e.Parent.Parent
-                    local c = v:GetChildren()
-                    for i, va in pairs(c) do
-                        local s = va.Spot
-                        va.Name = tostring(s.Value)
-                    end
-                    v[SecondDiamondCup].Detail.Color = Color3.fromRGB(0,191,255)
-                    v[SecondDiamondCup].Primary.Color = Color3.fromRGB(0,191,248)
-                end
-            end
-        end  
+    
+    local diamondmesh = Instance.new("MeshPart")
+    diamondmesh.Anchored = true
+    diamondmesh.MeshId = "rbxassetid://10241207391"
+    diamondmesh.Name = "fakediamond1"
+    diamondmesh.Size = Vector3.new(0.507319, 0.543082, 0.585802)
+    Instance.new("Highlight", diamondmesh)
+    
+    local player = game.Players.LocalPlayer
+    local character = player.character
+    local arena
+    local cupFolder
+    
+    for _, v in pairs(game:GetService("Workspace").ArenasREAL:GetDescendants()) do
+        if v:IsA("TextLabel") and v.Text == "@"..player.Name then
+            arena = v.Parent.Parent.Parent.Parent.Parent.Parent.Parent
+        end
+    end
+    
+    for _, v in pairs(arena:GetDescendants()) do
+        if v.Name == "Spot" then
+            v.Parent.Name = tostring(v.Value)
+            cupFolder = v.Parent.Parent
+        end
+    end
+    
+    for _,diamond in pairs(arena.ArenaTemplate.Important.Diamonds:GetChildren()) do
+        if diamond.Transparency == 1 then continue end
+        
+        local closest,distance = nil, 1000
+        for _,cup in pairs(cupFolder:GetChildren()) do
+            local mag = (cup.Detail.Position - diamond.Position).Magnitude
+            if mag < distance then closest = cup distance = mag end
+        end
+        
+        local clone = diamondmesh:Clone()
+        clone.Parent = workspace
+        clone.Position = closest.Primary.Position
+        closest.Primary:GetPropertyChangedSignal("Position"):Connect(function()
+            clone.Position = closest.Primary.Position
+        end)
     end
     end)
 end
