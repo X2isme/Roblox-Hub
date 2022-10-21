b5ecd34b2867664cccaf9864124774f148be1062
X2isme <98345602+X2isme@users.noreply.github.com>
2022-10-21 15:04:53 -0700

    Create RagdollSuperBreak.lua

---
+++ RagdollSuperBreak.lua
@@ -0,0 +1,558 @@
+local args = {
+    [1] = {
+        ["LeftFoot Cuneiform Bone 3"] = {
+            ["ImpactSpeed"] = 54.67616271972656
+        },
+        ["RightFoot Navicular Bone"] = {
+            ["ImpactSpeed"] = 139.53152465820312
+        },
+        ["LeftFoot"] = {
+            ["ImpactSpeed"] = 54.67616271972656
+        },
+        ["RightFoot Metatarsals Bone 2"] = {
+            ["ImpactSpeed"] = 139.53152465820312
+        },
+        ["Thoracic vertebrae Bone 8"] = {
+            ["ImpactSpeed"] = 89.51899719238281
+        },
+        ["LeftHand Carpal Bone 4"] = {
+            ["ImpactSpeed"] = 97.04644775390625
+        },
+        ["LeftHand Finger bone 14"] = {
+            ["ImpactSpeed"] = 67.17707061767578
+        },
+        ["Rib Bone 20"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["RightHand Carpal Bone 8"] = {
+            ["ImpactSpeed"] = 68.40251922607422
+        },
+        ["Facial Bone 11"] = {
+            ["ImpactSpeed"] = 73.22132110595703
+        },
+        ["RightHand Finger bone 8"] = {
+            ["ImpactSpeed"] = 68.40251922607422
+        },
+        ["Head"] = {
+            ["ImpactSpeed"] = 73.22132110595703
+        },
+        ["RightHand Finger bone 10"] = {
+            ["ImpactSpeed"] = 78.9847183227539
+        },
+        ["Facial Bone 7"] = {
+            ["ImpactSpeed"] = 73.22132110595703
+        },
+        ["LeftFoot Cuneiform Bone 1"] = {
+            ["ImpactSpeed"] = 54.67616271972656
+        },
+        ["LeftHand Metacarpal Bone 2"] = {
+            ["ImpactSpeed"] = 67.17707061767578
+        },
+        ["LeftFoot Toe Bone 3"] = {
+            ["ImpactSpeed"] = 54.67616271972656
+        },
+        ["Thoracic vertebrae Bone 10"] = {
+            ["ImpactSpeed"] = 89.51899719238281
+        },
+        ["Rib Bone 15"] = {
+            ["ImpactSpeed"] = 89.51899719238281
+        },
+        ["RightHand"] = {
+            ["ImpactSpeed"] = 68.40251922607422
+        },
+        ["RightHand Finger bone 3"] = {
+            ["ImpactSpeed"] = 68.40251922607422
+        },
+        ["Cervical vertebrae Bone 5"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["Rib Bone 11"] = {
+            ["ImpactSpeed"] = 89.51899719238281
+        },
+        ["Rib Bone 16"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["RightFoot Cuneiform Bone 2"] = {
+            ["ImpactSpeed"] = 139.53152465820312
+        },
+        ["Facial Bone 13"] = {
+            ["ImpactSpeed"] = 97.7037582397461
+        },
+        ["RightFoot Toe Bone 4"] = {
+            ["ImpactSpeed"] = 139.53152465820312
+        },
+        ["Rib Bone 1"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["Auditory Bone 4"] = {
+            ["ImpactSpeed"] = 73.22132110595703
+        },
+        ["RightHand Carpal Bone 7"] = {
+            ["ImpactSpeed"] = 68.40251922607422
+        },
+        ["RightFoot Metatarsals Bone 5"] = {
+            ["ImpactSpeed"] = 139.53152465820312
+        },
+        ["Cervical vertebrae Bone 4"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["LeftHand Finger bone 11"] = {
+            ["ImpactSpeed"] = 67.17707061767578
+        },
+        ["Cranial Bone 6"] = {
+            ["ImpactSpeed"] = 73.22132110595703
+        },
+        ["Rib Bone 7"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["RightHand Finger bone 6"] = {
+            ["ImpactSpeed"] = 78.9847183227539
+        },
+        ["Rib Bone 9"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["RightHand Metacarpal Bone 3"] = {
+            ["ImpactSpeed"] = 78.9847183227539
+        },
+        ["RightHand Carpal Bone 1"] = {
+            ["ImpactSpeed"] = 68.40251922607422
+        },
+        ["RightFoot Toe Bone 1"] = {
+            ["ImpactSpeed"] = 139.53152465820312
+        },
+        ["Cervical vertebrae Bone 2"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["LeftHand Finger bone 13"] = {
+            ["ImpactSpeed"] = 97.04644775390625
+        },
+        ["Coccygeal vertebrae Bone"] = {
+            ["ImpactSpeed"] = 89.51899719238281
+        },
+        ["RightFoot Metatarsals Bone 3"] = {
+            ["ImpactSpeed"] = 139.53152465820312
+        },
+        ["Left Scapula Bone"] = {
+            ["ImpactSpeed"] = 111.22811889648438
+        },
+        ["RightFoot"] = {
+            ["ImpactSpeed"] = 139.53152465820312
+        },
+        ["Rib Bone 24"] = {
+            ["ImpactSpeed"] = 89.51899719238281
+        },
+        ["Facial Bone 1"] = {
+            ["ImpactSpeed"] = 228.07960510253906
+        },
+        ["Left Humerus Bone"] = {
+            ["ImpactSpeed"] = 111.22811889648438
+        },
+        ["UpperTorso"] = {
+            ["ImpactSpeed"] = 89.51899719238281
+        },
+        ["LeftHand Carpal Bone 7"] = {
+            ["ImpactSpeed"] = 67.17707061767578
+        },
+        ["Auditory Bone 5"] = {
+            ["ImpactSpeed"] = 73.22132110595703
+        },
+        ["LeftFoot Cuneiform Bone 2"] = {
+            ["ImpactSpeed"] = 54.67616271972656
+        },
+        ["RightHand Finger bone 9"] = {
+            ["ImpactSpeed"] = 68.40251922607422
+        },
+        ["Thoracic vertebrae Bone 7"] = {
+            ["ImpactSpeed"] = 89.51899719238281
+        },
+        ["LeftHand Finger bone 3"] = {
+            ["ImpactSpeed"] = 67.17707061767578
+        },
+        ["Auditory Bone 2"] = {
+            ["ImpactSpeed"] = 73.22132110595703
+        },
+        ["Cranial Bone 7"] = {
+            ["ImpactSpeed"] = 228.07960510253906
+        },
+        ["RightFoot Toe Bone 2"] = {
+            ["ImpactSpeed"] = 139.53152465820312
+        },
+        ["Thoracic vertebrae Bone 1"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["Left Ulna Bone"] = {
+            ["ImpactSpeed"] = 105.08999633789062
+        },
+        ["Right Ulna Bone"] = {
+            ["ImpactSpeed"] = 80.57195281982422
+        },
+        ["Hyoid Bone"] = {
+            ["ImpactSpeed"] = 228.07960510253906
+        },
+        ["Cervical vertebrae Bone 7"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["RightHand Carpal Bone 2"] = {
+            ["ImpactSpeed"] = 68.40251922607422
+        },
+        ["RightFoot Cuneiform Bone 3"] = {
+            ["ImpactSpeed"] = 139.53152465820312
+        },
+        ["Sacrum Bone 4"] = {
+            ["ImpactSpeed"] = 89.51899719238281
+        },
+        ["Left Fibula"] = {
+            ["ImpactSpeed"] = 114.76152038574219
+        },
+        ["RightHand Finger bone 14"] = {
+            ["ImpactSpeed"] = 68.40251922607422
+        },
+        ["LeftHand Carpal Bone 1"] = {
+            ["ImpactSpeed"] = 97.04644775390625
+        },
+        ["LeftFoot Metatarsals Bone 2"] = {
+            ["ImpactSpeed"] = 116.53804779052734
+        },
+        ["Rib Bone 17"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["LeftHand Finger bone 6"] = {
+            ["ImpactSpeed"] = 67.17707061767578
+        },
+        ["Left Tibia"] = {
+            ["ImpactSpeed"] = 114.76152038574219
+        },
+        ["Sacrum Bone 1"] = {
+            ["ImpactSpeed"] = 89.51899719238281
+        },
+        ["LeftFoot Metatarsals Bone 5"] = {
+            ["ImpactSpeed"] = 116.53804779052734
+        },
+        ["Sacrum Bone 3"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["Sacrum Bone 5"] = {
+            ["ImpactSpeed"] = 89.51899719238281
+        },
+        ["Facial Bone 12"] = {
+            ["ImpactSpeed"] = 76.96621704101562
+        },
+        ["Hip Bone 4"] = {
+            ["ImpactSpeed"] = 103.68026733398438
+        },
+        ["Cervical vertebrae Bone 1"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["Rib Bone 6"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["Rib Bone 19"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["Rib Bone 13"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["Left Radius Bone"] = {
+            ["ImpactSpeed"] = 225.73081970214844
+        },
+        ["RightHand Finger bone 12"] = {
+            ["ImpactSpeed"] = 78.9847183227539
+        },
+        ["LeftFoot Toe Bone 1"] = {
+            ["ImpactSpeed"] = 104.74636840820312
+        },
+        ["Thoracic vertebrae Bone 3"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["RightHand Finger bone 7"] = {
+            ["ImpactSpeed"] = 68.40251922607422
+        },
+        ["RightHand Finger bone 2"] = {
+            ["ImpactSpeed"] = 78.9847183227539
+        },
+        ["Cranial Bone 1"] = {
+            ["ImpactSpeed"] = 76.96621704101562
+        },
+        ["Auditory Bone 3"] = {
+            ["ImpactSpeed"] = 73.22132110595703
+        },
+        ["LeftHand Finger bone 12"] = {
+            ["ImpactSpeed"] = 67.17707061767578
+        },
+        ["Facial Bone 2"] = {
+            ["ImpactSpeed"] = 73.22132110595703
+        },
+        ["LeftFoot Toe Bone 2"] = {
+            ["ImpactSpeed"] = 104.74636840820312
+        },
+        ["LeftHand Finger bone 10"] = {
+            ["ImpactSpeed"] = 67.17707061767578
+        },
+        ["LeftFoot Metatarsals Bone 4"] = {
+            ["ImpactSpeed"] = 54.67616271972656
+        },
+        ["LeftHand Finger bone 2"] = {
+            ["ImpactSpeed"] = 67.17707061767578
+        },
+        ["Rib Bone 10"] = {
+            ["ImpactSpeed"] = 89.51899719238281
+        },
+        ["LeftFoot Toe Bone 4"] = {
+            ["ImpactSpeed"] = 104.74636840820312
+        },
+        ["Cervical vertebrae Bone 3"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["Cranial Bone 3"] = {
+            ["ImpactSpeed"] = 76.96621704101562
+        },
+        ["LeftFoot Metatarsals Bone 3"] = {
+            ["ImpactSpeed"] = 54.67616271972656
+        },
+        ["RightHand Carpal Bone 6"] = {
+            ["ImpactSpeed"] = 78.9847183227539
+        },
+        ["LeftHand"] = {
+            ["ImpactSpeed"] = 67.17707061767578
+        },
+        ["RightFoot Toe Bone 3"] = {
+            ["ImpactSpeed"] = 139.53152465820312
+        },
+        ["Facial Bone 8"] = {
+            ["ImpactSpeed"] = 73.22132110595703
+        },
+        ["LeftFoot Cuboid Bone"] = {
+            ["ImpactSpeed"] = 54.67616271972656
+        },
+        ["RightHand Metacarpal Bone 2"] = {
+            ["ImpactSpeed"] = 78.9847183227539
+        },
+        ["Right Scapula Bone"] = {
+            ["ImpactSpeed"] = 129.68240356445312
+        },
+        ["RightUpperArm"] = {
+            ["ImpactSpeed"] = 129.68240356445312
+        },
+        ["LeftUpperArm"] = {
+            ["ImpactSpeed"] = 111.22811889648438
+        },
+        ["Auditory Bone 6"] = {
+            ["ImpactSpeed"] = 73.22132110595703
+        },
+        ["Cervical vertebrae Bone 6"] = {
+            ["ImpactSpeed"] = 89.51899719238281
+        },
+        ["Cranial Bone 5"] = {
+            ["ImpactSpeed"] = 73.22132110595703
+        },
+        ["Rib Bone 4"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["RightHand Carpal Bone 3"] = {
+            ["ImpactSpeed"] = 68.40251922607422
+        },
+        ["LeftHand Carpal Bone 8"] = {
+            ["ImpactSpeed"] = 67.17707061767578
+        },
+        ["Sacrum Bone 2"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["Rib Bone 3"] = {
+            ["ImpactSpeed"] = 89.51899719238281
+        },
+        ["Hyoid bone"] = {
+            ["ImpactSpeed"] = 89.51899719238281
+        },
+        ["LowerTorso"] = {
+            ["ImpactSpeed"] = 103.68026733398438
+        },
+        ["LeftFoot Calcaneus Bone"] = {
+            ["ImpactSpeed"] = 104.74636840820312
+        },
+        ["RightFoot Talus Bone"] = {
+            ["ImpactSpeed"] = 139.53152465820312
+        },
+        ["LeftUpperLeg"] = {
+            ["ImpactSpeed"] = 57.275230407714844
+        },
+        ["Cranial Bone 4"] = {
+            ["ImpactSpeed"] = 73.22132110595703
+        },
+        ["Right Humerus Bone"] = {
+            ["ImpactSpeed"] = 129.68240356445312
+        },
+        ["RightFoot Toe Bone 5"] = {
+            ["ImpactSpeed"] = 139.53152465820312
+        },
+        ["Thoracic vertebrae Bone 6"] = {
+            ["ImpactSpeed"] = 89.51899719238281
+        },
+        ["Rib Bone 21"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["Rib Bone 23"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["LeftHand Carpal Bone 5"] = {
+            ["ImpactSpeed"] = 67.17707061767578
+        },
+        ["Thoracic vertebrae Bone 12"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["Right Tibia"] = {
+            ["ImpactSpeed"] = 155.43991088867188
+        },
+        ["LeftLowerLeg"] = {
+            ["ImpactSpeed"] = 114.76152038574219
+        },
+        ["RightHand Finger bone 13"] = {
+            ["ImpactSpeed"] = 78.9847183227539
+        },
+        ["Lumbar vertebrae Bone 4"] = {
+            ["ImpactSpeed"] = 89.51899719238281
+        },
+        ["Rib Bone 18"] = {
+            ["ImpactSpeed"] = 89.51899719238281
+        },
+        ["LeftHand Finger bone 8"] = {
+            ["ImpactSpeed"] = 67.17707061767578
+        },
+        ["Rib Bone 12"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["LeftFoot Navicular Bone"] = {
+            ["ImpactSpeed"] = 116.53804779052734
+        },
+        ["LeftHand Metacarpal Bone 1"] = {
+            ["ImpactSpeed"] = 97.04644775390625
+        },
+        ["RightHand Carpal Bone 4"] = {
+            ["ImpactSpeed"] = 68.40251922607422
+        },
+        ["Rib Bone 5"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["RightLowerArm"] = {
+            ["ImpactSpeed"] = 80.57195281982422
+        },
+        ["Facial Bone 6"] = {
+            ["ImpactSpeed"] = 73.22132110595703
+        },
+        ["LeftHand Metacarpal Bone 4"] = {
+            ["ImpactSpeed"] = 97.04644775390625
+        },
+        ["Cranial Bone 8"] = {
+            ["ImpactSpeed"] = 97.7037582397461
+        },
+        ["LeftFoot Talus Bone"] = {
+            ["ImpactSpeed"] = 104.74636840820312
+        },
+        ["RightUpperLeg"] = {
+            ["ImpactSpeed"] = 151.33824157714844
+        },
+        ["Facial Bone 5"] = {
+            ["ImpactSpeed"] = 73.22132110595703
+        },
+        ["Facial Bone 14"] = {
+            ["ImpactSpeed"] = 97.7037582397461
+        },
+        ["LeftLowerArm"] = {
+            ["ImpactSpeed"] = 51.88727951049805
+        },
+        ["Facial Bone 4"] = {
+            ["ImpactSpeed"] = 73.22132110595703
+        },
+        ["Thoracic vertebrae Bone 9"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["LeftHand Metacarpal Bone 5"] = {
+            ["ImpactSpeed"] = 67.17707061767578
+        },
+        ["Right Fibula"] = {
+            ["ImpactSpeed"] = 155.43991088867188
+        },
+        ["Facial Bone 10"] = {
+            ["ImpactSpeed"] = 73.22132110595703
+        },
+        ["Thoracic vertebrae Bone 11"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["LeftHand Finger bone 5"] = {
+            ["ImpactSpeed"] = 67.17707061767578
+        },
+        ["Rib Bone 22"] = {
+            ["ImpactSpeed"] = 89.51899719238281
+        },
+        ["Facial Bone 3"] = {
+            ["ImpactSpeed"] = 76.96621704101562
+        },
+        ["RightHand Finger bone 4"] = {
+            ["ImpactSpeed"] = 68.40251922607422
+        },
+        ["LeftHand Finger bone 1"] = {
+            ["ImpactSpeed"] = 97.04644775390625
+        },
+        ["RightLowerLeg"] = {
+            ["ImpactSpeed"] = 155.43991088867188
+        },
+        ["Rib Bone 8"] = {
+            ["ImpactSpeed"] = 89.51899719238281
+        },
+        ["Auditory Bone 1"] = {
+            ["ImpactSpeed"] = 97.7037582397461
+        },
+        ["Thoracic vertebrae Bone 2"] = {
+            ["ImpactSpeed"] = 89.51899719238281
+        },
+        ["Hip Bone 2"] = {
+            ["ImpactSpeed"] = 103.68026733398438
+        },
+        ["Cranial Bone 2"] = {
+            ["ImpactSpeed"] = 73.22132110595703
+        },
+        ["LeftHand Finger bone 9"] = {
+            ["ImpactSpeed"] = 67.17707061767578
+        },
+        ["Thoracic vertebrae Bone 5"] = {
+            ["ImpactSpeed"] = 146.51199340820312
+        },
+        ["RightFoot Metatarsals Bone 4"] = {
+            ["ImpactSpeed"] = 139.53152465820312
+        },
+        ["RightFoot Cuneiform Bone 1"] = {
+            ["ImpactSpeed"] = 139.53152465820312
+        },
+        ["LeftFoot Toe Bone 5"] = {
+            ["ImpactSpeed"] = 54.67616271972656
+        },
+        ["LeftFoot Metatarsals Bone 1"] = {
+            ["ImpactSpeed"] = 104.74636840820312
+        },
+        ["RightHand Carpal Bone 5"] = {
+            ["ImpactSpeed"] = 68.40251922607422
+        },
+        ["Left Clavicles Bone"] = {
+            ["ImpactSpeed"] = 111.22811889648438
+        },
+        ["RightHand Finger bone 1"] = {
+            ["ImpactSpeed"] = 78.9847183227539
+        },
+        ["RightHand Metacarpal Bone 1"] = {
+            ["ImpactSpeed"] = 68.40251922607422
+        },
+        ["Facial Bone 9"] = {
+            ["ImpactSpeed"] = 76.96621704101562
+        },
+        ["Right Radius Bone"] = {
+            ["ImpactSpeed"] = 82.39868927001953
+        },
+        ["Lumbar vertebrae Bone 5"] = {
+            ["ImpactSpeed"] = 89.51899719238281
+        }
+    },
+    [2] = 10000,
+    [3] = 10000
+}
+
+game:GetService("ReplicatedStorage").Events.SummarizeStatus:FireServer(unpack(args))
+game:GetService("ReplicatedStorage").Events.RestartPlayerEvent:FireServer()
