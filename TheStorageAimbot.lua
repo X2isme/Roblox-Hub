fd60bf225bfa9dac50d6781d12cff9707f2b452d
X2isme <98345602+X2isme@users.noreply.github.com>
2025-04-30 16:04:58 -0700

    Update TheStorageAimbot.lua

--- TheStorageAimbot.lua
+++ TheStorageAimbot.lua
@@ -11,10 +11,13 @@ local RemoteEvent = ReplicatedStorage:WaitForChild("RemoteEvent")
 local UserInputService = game:GetService("UserInputService")
 
 local AimbotLabel = Instance.new("TextLabel")
-AimbotLabel.Size = UDim2.new(0, 200, 0, 50)
-AimbotLabel.Position = UDim2.new(0.8, 0, 0.125, 0)
+AimbotLabel.Size = UDim2.new(0.08, 0, 0.05, 0)
+AimbotLabel.Position = UDim2.new(0.77, 0, 0.09, 0)
 AimbotLabel.TextColor3 = Color3.fromRGB(255, 0, 0)
+AimbotLabel.Scaled = true
 AimbotLabel.Text = "Aimbot: OFF"
+AimbotLabel.BackgroundTransparency = 0
+A
 AimbotLabel.Parent = game:GetService("Players").LocalPlayer.PlayerGui.HUD
 
 local function Start()
